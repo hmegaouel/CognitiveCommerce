113ce891b4144f50252b045d86a9922eb58f7cdf
mohamedkadri <mohamedkadri.ecp@gmail.com>
2017-03-07 21:05:42 +0100

    Update Profile.java

--- src/main/java/personality/Profile.java
+++ src/main/java/personality/Profile.java
@@ -11,7 +11,7 @@
  * specific language governing permissions and limitations under the License.
  */
 
-
+package personality;
 import java.util.List;
 
 import com.google.gson.annotations.SerializedName;
