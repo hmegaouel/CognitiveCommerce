2fed9273e600f517a8e7dd780fdd4fc1280a4540
mohamedkadri <mohamedkadri.ecp@gmail.com>
2017-03-07 19:03:22 +0100

    Add files via upload

---
+++ src/main/java/personality/Personalityinsightsmodel/Element.java
@@ -0,0 +1,32 @@
+package Personalityinsightsmodel;
+
+import java.util.Date;
+
+public class Element {
+	
+	public Date date;
+	public double sentiment;
+	
+	public Element(double s, Date d) {
+		this.date = d;
+		this.sentiment = s;
+	}
+	
+	
+	public Element (double s){
+		this.sentiment=s;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public void setSentiment(double sent) {
+		this.sentiment = sent;
+	}
+	
+	public String toString() {
+		return "{"+sentiment+";"+date+"}";
+	}
+
+}
